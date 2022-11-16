e94aba9c69f9dd09732d4a740659f8bf1d235d6c
morganecf <morganeciot@gmail.com>
2022-11-15 21:11:09 -0800

    Debug deploy-functions.sh

--- scripts/deploy-functions.sh
+++ scripts/deploy-functions.sh
@@ -30,31 +30,31 @@ CROMWELL_SA_TOKEN=$(curl -sH "Authorization: Bearer ${TOKEN}" \
 
 echo $CROMWELL_SA_TOKEN
 
-#   | python3 -c 'import json,sys; print(json.load(sys.stdin)["accessToken"])')
+# Register the SA with Sam
+curl -sH "Authorization: Bearer ${CROMWELL_SA_TOKEN}" "https://sam.dsde-prod.broadinstitute.org/register/user/v1" -d ""
 
-# # Register the SA with Sam
-# curl -sH "Authorization: Bearer ${CROMWELL_SA_TOKEN}" "https://sam.dsde-prod.broadinstitute.org/register/user/v1" -d ""
-
-# # Allow SA to start workflows in the collection
-# curl -sH "Authorization: Bearer ${TOKEN}" -X PUT "https://sam.dsde-prod.broadinstitute.org/api/resources/v1/workflow-collection/${COLLECTION}/policies/writer" -H "Content-Type: application/json" -d "{\"memberEmails\": [\"${CROMWELL_SA}\"], \"roles\": [\"writer\"], \"actions\": []}"
+# Allow SA to start workflows in the collection
+curl -sH "Authorization: Bearer ${TOKEN}" -X PUT "https://sam.dsde-prod.broadinstitute.org/api/resources/v1/workflow-collection/${COLLECTION}/policies/writer" -H "Content-Type: application/json" -d "{\"memberEmails\": [\"${CROMWELL_SA}\"], \"roles\": [\"writer\"], \"actions\": []}"
  
-# # Create key for SA 
-# CROMWELL_SA_KEY=$(gcloud iam service-accounts keys create /dev/stdout --iam-account "${CROMWELL_SA}")
+# Create key for SA 
+CROMWELL_SA_KEY=$(gcloud iam service-accounts keys create /dev/stdout --iam-account "${CROMWELL_SA}")
 
-# # Deploy Cromwell launcher function, passing in key as environment variable
-# # TODO non-destructively use --update-env-vars instead?
-# gcloud functions deploy python-http-function \
-#     --gen2 \
-#     --runtime=python310 \
-#     --region=$REGION \
-#     --source=. \
-#     --entry-point=launch_cromwell \
-#     --trigger-http \
-#     --allow-unauthenticated
-#     --set-env-vars KEY=$CROMWELL_SA_KEY
+echo $CROMWELL_SA_KEY
 
-# # NOTE: Could also store key in runtime config so that functions can access it
-# # gcloud beta runtime-config configs variables set --config-name=cromwell-config cromwell-key $CROMWELL_SA_KEY
+# Deploy Cromwell launcher function, passing in key as environment variable
+# TODO non-destructively use --update-env-vars instead?
+gcloud functions deploy python-http-function \
+    --gen2 \
+    --runtime=python310 \
+    --region=$REGION \
+    --source=. \
+    --entry-point=launch_cromwell \
+    --trigger-http \
+    --allow-unauthenticated
+    --set-env-vars KEY=$CROMWELL_SA_KEY
 
-# # Deploy Cromwell parser functions
-# # TODO
+# NOTE: Could also store key in runtime config so that functions can access it
+# gcloud beta runtime-config configs variables set --config-name=cromwell-config cromwell-key $CROMWELL_SA_KEY
+
+# Deploy Cromwell parser functions
+# TODO
