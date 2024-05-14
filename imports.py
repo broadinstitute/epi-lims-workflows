03e51dabd5b754e61d47502f313b0a633fb28d9b
Kevin Dong <kdong@broadinstitute.org>
2024-05-14 11:25:31 -0400

    Adding queries to find and overwrite existing subjects

--- imports.py
+++ imports.py
@@ -27,6 +27,59 @@ def import_subjects(project, username, password, subject_type, data):
         return response.json()
 
 
+def query_subjects(project, username, password, subject_type, query):
+    project = "dev-" if "dev" in project else ""
+    params = {
+        "method": "subjects",
+        "subject_type": subject_type,
+        "query": query,
+        "limit": "5000",
+        "username": username,
+        "password": password,
+    }
+    try:
+        response = requests.get(
+            "https://lims.{0}epi.broadinstitute.org/api".format(project),
+            params=params,
+            headers={"Content-Type": "application/json"},
+        )
+        print(f"Response: {response}")
+    except HTTPError as http_err:
+        print(f"HTTP error occurred: {http_err}")
+    except Exception as err:
+        print(f"Error: {err}")
+    else:
+        return response.json()
+
+
+def parse_query(response, udf_names):
+    result = {}
+    for subject in response["Subjects"]:
+        udf_values = {}
+        for udf in subject["udfs"]:
+            if udf["name"] in udf_names:
+                if isinstance(udf["value"], dict):
+                    udf_values[udf["name"]] = udf["value"]["name"]
+                else:
+                    udf_values[udf["name"]] = udf["value"]
+        result[str(subject["id"])] = udf_values
+    return result
+
+
+def check_subjects(parsed_query, search_udfs):
+    matching_subjects = []
+    for uid, udfs in parsed_query.items():
+        if udfs == search_udfs:
+            matching_subjects.append(uid)
+    if len(matching_subjects) == 0:
+        return {}
+    elif len(matching_subjects) > 1:
+        subject_names = ', '.join(subject for subject in matching_subjects)
+        raise ValueError(f"Multiple existing subjects found: {subject_names}")
+    else:
+        return {"UID": matching_subjects[0]}
+
+
 def import_alignments(project, username, password, alignments, ref_seq, commands):
     lims_alignments = []
     for alignment in alignments:
@@ -143,6 +196,13 @@ def import_lanes(project, username, password, context, outputs):
     lims_lanes = []
     lanes = outputs["laneOutputs"]
     lane_type = "Paired" if lanes[0]["libraryOutputs"][0]["read2"] else "Single"
+    # Query for existing lanes
+    flowcell = outputs["flowcellId"]
+    lims_query = "\"Flow Cell\" = '{}'".format(flowcell)
+    udf_names = ["Flow Cell", "Lane-of-FC"]
+    query_response = query_subjects(project, username, password, "LIMS_Lane", lims_query)
+    print(query_response)
+    parsed_response = parse_query(query_response, udf_names)
     for lane_output in outputs["laneOutputs"]:
         lims_lanes.append(
             {
@@ -155,10 +215,34 @@ def import_lanes(project, username, password, context, outputs):
                 "Lane Type": lane_type
             }
         )
-    return import_subjects(project, username, password, "LIMS_Lane", lims_lanes)
+        search_udfs = {
+            "Flow Cell": outputs["flowcellId"],
+            "Lane-of-FC": str(lane_output["lane"])
+        }
+        uid_dict = check_subjects(parsed_response, search_udfs)
+        lims_lanes[-1].update(uid_dict)
+    uid_names = {str(d['id']): d['name'] for d in query_response['Subjects']}
+    search_uids = [d.get('UID', '{}') for d in lims_lanes]
+    search_names = ','.join([uid_names.get(uid, '{}') for uid in search_uids])
+    import_response = import_subjects(project, username, password, "LIMS_Lane", lims_lanes)
+    print(import_response)
+    imported_names = (import_response['names'].split(','))
+    # imported_names = (
+    #     [*import_response['names']] if isinstance(import_response['names'], list) else
+    #     [import_response['names']]
+    # )
+    all_names = search_names.format(*imported_names).split(',')
+    return(all_names)
 
 def import_ss_lane_subsets(project, username, password, context, outputs, lims_lanes):
-    for lane_output, lims_lane in zip(outputs["laneOutputs"], lims_lanes['names'].split(',')):   
+    # Query for existing lanes
+    pa_uid = context["poolAliquotUID"]
+    lims_query = "\"SS-CoPA\"->\"SS-PA\"->id = {}".format(pa_uid)
+    udf_names = ["SS-CoPA", "LIMS_Lane"]
+    query_response = query_subjects(project, username, password, "SS-LS", lims_query)
+    print(query_response)
+    parsed_response = parse_query(query_response, udf_names)
+    for lane_output, lims_lane in zip(outputs["laneOutputs"], lims_lanes):   
         lane_subsets = []
         buffer = []
         for library_output in lane_output["libraryOutputs"]:
@@ -173,16 +257,19 @@ def import_ss_lane_subsets(project, username, password, context, outputs, lims_l
                 "PF Fragments (BC)": library_output["pfFragments"],
                 "DEMUX Version": outputs["pipelineVersion"]
             })
+            search_udfs = {
+                "SS-CoPA": library_output["name"],
+                "LIMS_Lane": lims_lane
+            }
+            uid_dict = check_subjects(parsed_response, search_udfs)
+            buffer[-1].update(uid_dict)
             if len(buffer) == 10:
                 lane_subsets.append(buffer)
                 buffer = []  # Start a new buffer array
-            
         if buffer:
             lane_subsets.append(buffer)
-        
         for group in lane_subsets:
             print(import_subjects(project, username, password, "SS-LS", group))
-        
     return lane_subsets#import_subjects(project, username, password, "SS-LS", lane_subsets)
 
 def update_ss_pa(project, username, password, context, outputs):
